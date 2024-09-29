d451a33e83e061ae6bfbfb3873851a0ee27243b6
Dan <dannyallman4@gmail.comcd>
2024-09-29 02:18:07 -0700

    fixed for build

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ const App = () => {
   if (!cv) {
     throw new Promise<void>((resolve) => {
       // call the render loop as a promise fulfillment because this module is lorg
-      // @ts-expect-error
-      opencv().then((val: CV) => {
+      (opencv as () => Promise<CV>)().then((val: CV) => {
         console.log("opencv ready");
         // @ts-expect-error we are setting the cv object here... typescript is not happy
         cv = val;
